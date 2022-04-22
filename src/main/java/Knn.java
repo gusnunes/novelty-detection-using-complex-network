f0f344a87e61542a6fd5960bc3899b4afa078e95
gusnunes <daniloaugusto_nunes@hotmail.com>
2022-04-22 15:36:14 -0300

    calcula matriz de distancia dos micro-grupos

--- src/main/java/Knn.java
+++ src/main/java/Knn.java
@@ -1,31 +1,71 @@
 import moa.cluster.Cluster;
 import moa.cluster.Clustering;
 
+import org.apache.commons.lang3.tuple.Pair;
+import java.util.ArrayList;
+import java.util.Comparator;
+
 import org.jgrapht.*;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
 public class Knn {
     public int k;
-    public Clustering microClusters;
+    public Clustering microClustering;
 
-    public Knn(int k, Clustering microClusters){
+    public Knn(int k, Clustering microClustering){
         this.k = k;
-        this.microClusters = microClusters;
+        this.microClustering = microClustering;
     }
 
-    // por enquanto, só transformar cada micro-grupo em vertice de um grafo
-    // testar a biblioteca
     public void geraRede(){
-        Graph<Cluster, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+        Cluster microClusterA, microClusterB;
+        double[] centerA, centerB;
+        double[][] distance_matrix;
+        
+        double distance;
+        int clusters_number;
 
-        // Vertice para cada micro_grupo
-        for(Cluster microCluster: microClusters.getClustering()){
-            g.addVertex(microCluster);
+        Graph<Cluster, DefaultEdge> microClustersNetwork;
+        microClustersNetwork = new SimpleGraph<>(DefaultEdge.class);
+        
+        ArrayList<Pair<Integer,Double>> tuples = new ArrayList<>();
+        
+        clusters_number = microClustering.size();
+        distance_matrix = new double[clusters_number][clusters_number];
+        
+        // build the distance matrix between micro-clusters
+        for(int row=0; row<clusters_number; row++){
+            microClusterA = microClustering.get(row);
+
+            // vertex for each micro-cluster
+            microClustersNetwork.addVertex(microClusterA);
+            
+            for(int column=0; column<clusters_number; column++){
+                if(row != column){
+                    microClusterB = microClustering.get(column);
+                    
+                    if(column > row){
+                        centerA = microClusterA.getCenter();
+                        centerB = microClusterB.getCenter();
+
+                        distance = euclideanDistance(centerA,centerB);
+                        distance_matrix[row][column] = distance;
+                    }
+                    else { 
+                        distance = distance_matrix[column][row]; 
+                    }
+
+                    tuples.add(Pair.of(column,distance));
+                }
+            }
+
+            tuples.sort(Comparator.comparing(Pair::getRight));
+            tuples.clear();
         }
     }
 
-    private static double distance(double[] pointA, double [] pointB) {
+    private static double euclideanDistance(double[] pointA, double [] pointB) {
 		double distance = 0.0;
 		for (int i = 0; i < pointA.length; i++) {
 			double d = pointA[i] - pointB[i];
@@ -35,6 +75,6 @@ public class Knn {
 	}
 
     public static void main(String[] args){
-        System.out.println("Teste");
+        
     }
 }
