efa4a28a8abd42e2187bd51dd1f93dee817ade12
gusnunes <daniloaugusto_nunes@hotmail.com>
2022-05-30 19:13:35 -0300

    output pra facilitar leitura no codigo java

--- python_scripts/teste.py
+++ python_scripts/teste.py
@@ -3,13 +3,16 @@ from igraph import *
 
 def main():
     filename = sys.argv[1]
-    print(filename)
+    #print(filename)
 
     g = Graph.Read_Edgelist(filename).as_undirected()
 
     #g = Graph.Read_Edgelist(filename)
-    x = Graph.community_multilevel(g)
+    #Graph.community_fastgreedy(g)
+    communities = g.community_fastgreedy()
 
-    print(x)
+    for cluster in communities.as_clustering():
+        str_cluster = str(cluster)
+        print(str_cluster.strip("[]"))
 
 main()
